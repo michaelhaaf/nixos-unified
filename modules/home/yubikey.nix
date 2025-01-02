bdb4acc6d2fa19f98cedf1b0a8a30d4117100db0
michaelhaaf <michael.haaf@gmail.com>
2025-01-02 17:06:13 -0500

    some todos for yubikey and gpg

--- modules/home/yubikey.nix
+++ modules/home/yubikey.nix
@@ -3,4 +3,25 @@
   home.packages = with pkgs; [
     yubikey-personalization
   ];
+
+  # Might need to move this out of home to get udev rules.
+  # But, I want it home for non NixOS systems... TODO
+  # Inspired by: https://rzetterberg.github.io/yubikey-gpg-nixos.html
+  #
+  # programs.ssh.startAgent = false;
+  # services.pcscd.enable = true;
+  #
+  # environment.systemPackages = with pkgs; [
+  #   gnupg
+  #   yubikey-personalization
+  # ];
+  #
+  # environment.shellInit = ''
+  #   gpg-connect-agent /bye
+  #   export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  # '';
+  #
+  # services.udev.packages = with pkgs; [
+  #   yubikey-personalization
+  # ];
 }

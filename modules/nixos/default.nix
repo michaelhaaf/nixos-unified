962368a428ecb00ec0ab9e6f9b5d0abc1c91e271
michaelhaaf <michael.haaf@gmail.com>
2025-01-04 18:05:57 -0500

    put nix settings in right spot

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -8,7 +8,14 @@ let
 in
 {
   # These users can add Nix caches.
-  nix.settings.trusted-users = [ "root" "michael" ];
+  nix.settings.trusted-users = [ "root" "michael" ];{
+  nix.settings.trusted-public-keys = [
+    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+  ];
+  nix.settings.substituters = [
+    "https://hyprland.cachix.org"
+  ];
+}
 
   services.openssh.enable = true;
 }
